2da0865ab5cfac47ee226f82ef8cc4efd639fa1c
Samuel Caetano <sam.kaetano@gmail.com>
2017-02-25 00:20:57 -0300

    Mine followers from a given account in Twitter
    Data miner script. Using Twitter API, and OAuth 2.0 authentication, retrieves all available followers from a given account and inserts it properly into the database

--- retrieve/retrieveData.py
+++ retrieve/retrieveData.py
@@ -7,6 +7,7 @@ import time
 import sys
 import os
 import pickle
+import logging
 
 # Internal imports
 sys.path.insert(0, \
@@ -21,11 +22,11 @@ con = sql.connect('/home/r2d2/projeto_ext_vero/Engine/sql/TwitterA.db')
 
 # Search the followers from a given user
 def get_followers_current_user(arg, count):
-    # Receives a set with the current insurance's followers 
+    # Receives a set with the current insurance's company followers
     followers_ids = OAuth.twitter_api.followers.ids(\
         user_id = arg, count = count)
     
-    # Saves the ids from the current insurance's followers
+    # Saves the ids from the current insurance's company followers
     followers = followers_ids['ids']
     kwargs = dict()
     
@@ -37,15 +38,30 @@ def get_followers_current_user(arg, count):
             kwargs.update({'user_id':arg})
             kwargs.update({'count':count})
             kwargs.update({'cursor':followers_ids['next_cursor']})
-        except (IndexError), e:
-            print '\tget_followers_current_user() :: ', e
+        except IndexError as e:
+            m = """Unexpected index error in 
+            get_followers_current_user() \t""", \
+                time.asctime(time.localtime(time.time()))
+            print m
+            logging.warning(m)
             break
         try:
             # Do the search through the available >cursors<
             followers_ids = OAuth.twitter_api.followers.ids(**kwargs)
             followers = followers + followers_ids['ids']
-        except (twitter.api.TwitterHTTPError), e:
-            print '\tget_followers_current_user() :: ', e
+        except twitter.api.TwitterHTTPError as e:
+            if str(e).find('401'):
+                m = json.dumps({'code': 401, \
+                    'message': 'Unauthorized', 'account': arg}), \
+                        time.asctime(time.localtime(time.time()))
+                print m
+                logging.info(m)
+            elif str(e).find('429'):
+                m = json.dumps({'code': 429, \
+                    'message': 'Rate limit exceeded'}), \
+                        time.asctime(time.localtime(time.time()))
+                print m
+                logging.info(m)
             break
     return followers
 
@@ -55,26 +71,59 @@ def get_tweets_current_user(q, count, repeat):
     try:
         return OAuth.twitter_api.statuses.user_timeline(\
             user_id = q, count = count, trim_user = True)
-    except(Exception), e:
-        print '\tget_tweets_current_user() :: ', e
+    except twitter.api.TwitterHTTPError as e:
+        if str(e).find('401'):
+            m = json.dumps({'code': 401, \
+                'message': 'Unauthorized', 'account': q}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
+        elif str(e).find('429'):
+            m = json.dumps({'code': 429, \
+                'message': 'Rate limit exceeded'}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
 
 # Retrieves the account of the user given as parameter
 def get_user(arg):
     try:
         user = OAuth.twitter_api.users.lookup(user_id=arg)
         return user[0]
-    except(Exception), e:
-        print '\tget_user() :: ', e
+    except twitter.api.TwitterHTTPError as e:
+        if str(e).find('401'):
+            m = json.dumps({'code': 401, \
+                'message': 'Unauthorized', 'account': arg}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
+        elif str(e).find('429'):
+            m = json.dumps({'code': 429, \
+                'message': 'Rate limit exceeded'}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
 
 # Procura pela conta de um usuario passado por parametro
 def search_user(arg):
     try:
         return OAuth.twitter_api.users.search(q=arg)
-    except(Exception), e:
-        print '\tsearch_user() :: ', e
+    except twitter.api.TwitterHTTPError as e:
+        if str(e).find('401'):
+            m = json.dumps({'code': 401, \
+                'message': 'Unauthorized', 'account': arg}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
+        elif str(e).find('429'):
+            m = json.dumps({'code': 429, \
+                'message': 'Rate limit exceeded'}), \
+                    time.asctime(time.localtime(time.time()))
+            print m
+            logging.info(m)
 
 # Creates files with the followers' data, tweets and account
-# (respectively) from a given insurance
+# (respectively) from a given insurance company
 def seguradora_to_file(arg):
     str1 = str(arg)+"Followers.txt"
     str2 = str(arg)+"Tweets.txt"
@@ -92,7 +141,7 @@ def seguradora_to_file(arg):
     pickle.dump(get_user(arg), obj)
     obj.close()
 
-# Retrieve the followers from a given insurance from file
+# Retrieve the followers from a given insurance company from file
 def get_followers_from_file(id_name):
     print 'get_seguradora_from_file()'
     str1 = str(id_name)+"Followers.txt"
@@ -100,7 +149,7 @@ def get_followers_from_file(id_name):
     f = pickle.load(obj)
     return f
 
-# Pega os tweets da seguradora a partir do arquivo
+# Retrieve insurance's company tweets from file
 def get_tweets_from_file(id_name):
     print 'get_tweets_from_file()'
     str1 = str(id_name)+"Tweets.txt"
@@ -108,7 +157,7 @@ def get_tweets_from_file(id_name):
     f = pickle.load(obj)
     return f
 
-# Pega os dados da conta de uma seguradora a partir de um arquivo
+# Retrieve insurance's company account data from file
 def get_seguradora_from_file(id_name):
     print 'get_seguradora_from_file()'
     str1 = str(id_name)+".txt"
@@ -125,51 +174,51 @@ def delete_files(arg):
     os.remove(str1)
     os.remove(str2)
     os.remove(str3)
-    
-# Insere no banco de dados as infos referentes a seguradora corrente
+
+# Inserts into the database the infos about the current insurance company
 def insertSeguradoraFromFile(name):
     print 'insertSeguradoraFromFile()'
     tweets = get_tweets_from_file(name)
     seguradora = get_seguradora_from_file(name)
     insert(seguradora, tweets, 1, 0)
 
-# Insere as infos de cada seguidor de uma determinada seguradora
+# Inserts the info from each follower of a given insurance company
 def insertFollowerFromFile(id_seguradora, followers, followers_count,\
     followers_protected, followers_noTweets, followers_in, control):
     
     print 'insertFollowerFromFile()'
     
-    # Le do banco de dados todos os seguidores da seguradora...
-    #...corrente previamente cadastrados
+    # Reads from database the whole insurance's companies followers
+    # previously inserted
     already_in = [_[0] for _ in dbm.GetFollowerBySeg(id_seguradora)]
 
-    # Elimina dos seguidores a serem inseridos...
-    #...os seguidores, desta seguradora, ja inseridos
+    # Removes from followers to be inserted
+    # those, who belongs to this insurance company, already inserted
     followers = [_ for _ in followers if _ not in already_in]
-    
-    # Marca a posicao do atual seguidor.
-    # Indica cada indice da list de seguidores
+
+    # Marks the position of the present follower
+    # Indicates each index of a list of followers
     k = 0
     
-    # Se verdadeiro, entao ainda restam seguidores para cadastrar.
-    # Se falso, nao restam seguidores
-    resta_seguidor = True
+    # If true, then there are followers to be inserted yet
+    # If false, there are none
+    follower_left = True
     
     current_user = dict()
     
     for item in followers:
         if item == -1:
-            # Control para saber se todos os seguidores foram inseridos
-            control = control + 1
+            # >control< show if all the followers were correctly inserted
+            control += 1
     
     if control >= len(followers):
-        resta_seguidor = False
+        follower_left = False
         
-    if resta_seguidor:
+    # Recursion stop criterion
+    if follower_left:
         for follower in followers:
-            # Se follower for -1, entao quer dizer
-            # que esse follower ja foi
-            # cadastrado com sucesso. Nao entra
+            # If >follower == -1<, then follower already inserted
+            # Goes to >else<
             if follower != -1:
                 if available(follower):
                     time.sleep(1)
@@ -184,89 +233,102 @@ def insertFollowerFromFile(id_seguradora, followers, followers_count,\
                                 id_seguradora)
                             
                             followers_in = followers_in + 1
+                            
+                            m = json.dumps({'code': 200, \
+                                'message': 'Sucess!', \
+                                'account': follower}), \
+                                    time.asctime(time.localtime(time.time()))
+                                
+                            print m
+                            logging.info(m)
                         else:
                             followers_noTweets = followers_noTweets + 1
                             followers_count -= 1
                             control += 1
-                            print "%d ignorado por nao possuir tweets.\
-                            Tweets disponiveis %d"\
-                                % (follower, len(user_timeline))
+                            m = json.dumps({'code': 304, \
+                                'message': 'No tweets available', \
+                                'account': follower}), \
+                                    time.asctime(time.localtime(time.time()))
+                            print m
+                            logging.info(m)
                         
-                        # Se o seguidor for cadastrado com sucesso...
-                        #...entao sua posicao na 'list'...
-                        #...eh substituida por um -1
+                        # If follower correctly inserted then it's value 
+                        # in it's position in the list is replaced by -1
                         followers[k] = -1
-                    except (Exception), e:
-                        # Checa se o retorno eh do NoneType...
-                        #...que significa que o usuario esta protegido
+                    except Exception as e:
+                        # Check if returned content is NoneType
+                        # which means this follower is protected
                         if str(e).find('NoneType'):
                             
                             followers_protected += 1                            
                             followers[k] = -1
                             followers_count -= 1
                             control += 1
-                            print 'Nao autorizado ',
-                            
-                        print "%d nao inserido. Exception [%s]"\
-                            % (follower, e)
                         next
                 else:
-                    # Se o seguidor corrente da seguradora corrente ja estiver...
-                    #...cadastrado no banco de dados, entao so se insere...
-                    #...no banco a relacao usuario-seguradora. E nao se...
-                    #...insere todas as infos deste seguidor
-                    print follower
-                    if follower:
-                        insert_SegUser(follower, id_seguradora)
-                        followers_in = followers_in + 1
-                        followers[k] = -1
-                        next
+                    # If current follower from the current insurance company
+                    # had already been inserted, then it only inserts the 
+                    # user-insurance relantionship. 
+                    # And ignores the whole follower info
+                    InsertInsuranceFollower(follower, id_seguradora)
+                    followers_in = followers_in + 1
+                    followers[k] = -1
+                    next
             else:
-                print k,'pulou'
-            k = k + 1
-        # Chama a funcao recursivamente, de modo que a cada chamada
-        # a list followers eh passada com os seguidores que ainda
-        # restam para serem cadastrados
+                print k,' jumped'
+            k += 1
+        # Uses recurvise call of this function, in a way that
+        # >followers< is passed with those followers 
+        # who still need to be inserted
         insertFollowerFromFile(id_seguradora, followers,\
             followers_count, followers_protected,\
                 followers_noTweets, followers_in, control)
     else:
-        raise Exception('Usuarios disponiveis: ',followers_count,\
-            ' | Usuarios inseridos: ',followers_in,\
-                ' | Usuarios sem tweets: ',followers_noTweets,\
-                    ' | Usuarios protegidos: ', followers_protected)
-                   
-# Verifica se o seguidor corrente ja esta no banco.
-# Se esta, available() retorna False. Se nao esta, retorna True
+        
+        m = json.dumps({'code':200, \
+            'message': 'Finished!', \
+            'users': {
+                'available': followers_count,
+                'inserted': followers_in,
+                'no-tweet': followers_noTweets,
+                'protected': followers_protected}})
+        
+        # Outputs to log file
+        logging.info(m)
+        
+        raise Exception(m)
+
+# Verifies if current follower is already in the database;
+# If it is in, return false. otherwise, returns true
 def available(arg):
     cursor2 = con.cursor()
     cursor2.execute("""SELECT idUsuario FROM Usuario
         WHERE (idUsuario = ?)""",(arg,))
     for row in cursor2.fetchall():
-        for id in row:
-            if id == arg:
+        for _ in row:
+            if _ == arg:
                 return False
     return True
 
-# Verifica se o tweet corrente ja esta no banco de dados.
-# Se sim, retorna False, caso contrario retorna True
+# Verifies if current tweet is already in database
+# If it is in, returns false. otherwise returns true
 def availableTweet(arg):
     cursor = con.cursor()
     cursor.execute("""SELECT idTweet FROM Tweet
         WHERE (idTweet = ?)""", (arg,))
-    for rw in cursor.fetchall():
-        for id in rw:
-            if id == arg:
+    for row in cursor.fetchall():
+        for _ in row:
+            if _ == arg:
                 return False
     return True
 
-# Mesma verificacao de available() e availableTweet()
+# Same verification as >available()< and >availableTweet()<
 def availableHash(arg):
     cursor = con.cursor()
     cursor.execute("""SELECT hashtag_content FROM Hashtag
         WHERE (hashtag_content = ?)""", (arg,))
-    for rw in cursor.fetchall():
-        for _ in rw:
+    for row in cursor.fetchall():
+        for _ in row:
             if _ == arg:
                 return False
     return True
@@ -305,68 +367,98 @@ def insert(account, tweets, isSeguradora, account_seg):
                             Hashtag_Tweet (Hashtag_idHashtag, Tweet_idTweet)
                             VALUES (?,?)""", (idHashtag, i['id']))
     if isSeguradora == 1:
-        # Preenche tabelas Seguradora e Seguradora_Usuario
+        # Fills Seguradora and Seguradora_Usuario tables
         con.execute("""INSERT OR IGNORE INTO Seguradora
             (idSeguradora, followers_count, statuses_count) VALUES (?,?,?)""",\
-                (account['id'], account['followers_count'],\
-                    account['statuses_count']))
+            (account['id'], account['followers_count'],\
+            account['statuses_count']))
         con.execute("""INSERT OR IGNORE INTO Seguradora_Usuario
             (Seguradora_idSeguradora, Usuario_idUsuario) VALUES (?,?)""",\
-                (account['id'], account['id']))
+            (account['id'], account['id']))
     else:
         con.execute("""INSERT OR IGNORE INTO Seguradora_Usuario
             (Seguradora_idSeguradora, Usuario_idUsuario) VALUES (?,?)""",\
-                (account_seg, account['id']))
+            (account_seg, account['id']))
     con.commit()
 
-def insert_SegUser(account, account_seg):
+def InsertInsuranceFollower(account, account_seg):
     cursor = con.cursor()
-    cursor.execute("""SELECT (Seguradora_idSeguradora) FROM Seguradora_Usuario
-        WHERE Usuario_idUsuario = ?""", (account,))
+    cursor.execute('''SELECT (Seguradora_idSeguradora) FROM Seguradora_Usuario
+        WHERE Usuario_idUsuario = ?''', (account,))
     for row in cursor.fetchall():
-        for id in row[0]:
+        for id in row:
             if(id != account_seg):
                 con.execute("""INSERT OR IGNORE INTO Seguradora_Usuario
-                    (Seguradora_idSeguradora, Usuario_idUsuario) VALUES (?,?)""",\
-                        (account_seg, account))
+                (Seguradora_idSeguradora, Usuario_idUsuario) VALUES (?,?)""",\
+                (account_seg, account))
                 con.commit()
 
 def main():
     try:
-        is_Seg = 1 # 0, entao usuario; 1, entao seguradora
-        name = raw_input('Nome da conta: ')
+        name = raw_input('Account name: ')
         search_account = search_user(name)
         print 'Resuts:'
         for i, v in enumerate(search_account):
             print '%d | %s' % (i+1, v['name'])
             
-        option = input('Escolha a conta (1-10):')
+        option = input('Choose an account (1-10):')
         user = search_account[option-1]
         seguradora_to_file(user['id'])
+        
+        # Outputs to log file
+        logging.info('Getting %s information' % dbm.GetAccountLabel(user['id']))
         return user
-    except(twitter.api.TwitterHTTPError), e:
-        print 'Error in main() :: ', e
+    except twitter.api.TwitterHTTPError as e:
+        if str(e).find('401'):
+            m = json.dumps({'code': 401, \
+                'message': 'Unauthorized', 'account': user['id']})
+            print m
+            
+            # Outputs to log file
+            logging.info(m)
+            
+        elif str(e).find('429'):
+            m = json.dumps({'code': 429, \
+                'message': 'Rate limit exceeded'})
+            print m
+            
+            # Outputs to log file
+            logging.info(m)
         print 'Waiting API time'
         time.sleep(900)
+        return user
 
-usr = main() # Recebe os dados da seguradora assim que termina o main
+logging.basicConfig(filename = 'results.log', \
+    level = logging.DEBUG)
 
+# Receives the data from the insurance-company
+usr = main() 
+
+# Gets current time
 time_begin = time.asctime(time.localtime(time.time()))
-insertSeguradoraFromFile(usr['id']) # Insere a seguradora no banco de dados
-print 'Em execucao....'
+
+
+# Inserts the insurance-company in the database
+insertSeguradoraFromFile(usr['id']) 
+print 'In execution...'
 try:
-    # Recebe a quantidade de seguidores da seguradora
+    # Gets the quantity of followers from the 
+    # current insurance-company
     followers_count = usr['followers_count']
     
     followers = get_followers_from_file(usr['id'])
     
 except(twitter.api.TwitterHTTPError), e:
-    print 'Erro', e
+    print 'Error ', e
+    logging.warning(e)
 
 insertFollowerFromFile(usr['id'], followers, followers_count, 0, 0, 0, 0)
 
-print 'Iniciado as: ',time_begin
-print 'Finalizado as: ',time.asctime(time.localtime(time.time()))
+print 'Started at: ', time_begin
+print 'Done at: ', time.asctime(time.localtime(time.time()))
 
+# Delete used files
 delete_files(usr['id'])
+
+# Closes database connection
 con.close()
