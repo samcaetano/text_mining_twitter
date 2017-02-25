c7aa25269dcecc4c602564894e098d0084077ddd
Samuel Caetano <sam.kaetano@gmail.com>
2017-02-25 00:38:09 -0300

    Update preprocessing.py

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-    AUTOR: Samuel Caetano
-    DESCRICAO: 
-"""
+# This script was developed by Samuel Caetano
+# Preprocessing script. 
+# This script works on preprocessing the dataset available, cleaning document
+# content, removing unwanted tokens (such as twitter mentions, numbers, URLs,
+# HTMLs and hashtags), tokenizing, removing stop-words. In the final stage it
+# goes stemming and weighting them
 
 # External imports
 import nltk as NL 
@@ -22,52 +24,38 @@ sys.path.insert(0, '/home/r2d2/projeto_ext_vero/Engine/lib')
 
 import DatabaseMethods as dbm
 
-# Bloco para tf_idf : algoritmo base no site ->
-#...http://stevenloria.com/finding-important-words-in-a-document-using-tf-idf/
-#
+# For more term weighting go to:
+# http://stevenloria.com/finding-important-words-in-a-document-using-tf-idf/
 def Tf(term, tweet): 
-    # Term frequency : o numero de vezes que o term apareceu no
-    #...documento (tweet), dividido pelo numero total de termos no documento
-    #return 1+math.log10(float(tweet.count(term))/float(len(tweet)))
-    return float(tweet.count(term)) # puro
-    #return float(tweet.count(term))/float(len(tweet)) # normalizado
+    # Pure term frequency
+    return float(tweet.count(term))
 
-#2. n_contains : retorna o numero de documentos que contem o termo
-def NContains(word, tweets_tokenized):
-    return sum(1 for tweet in tweets_tokenized if word in tweet)
-
-#3. idf : 'inverse document frequency' : mede quao comum um termo eh no conjunto
-#com todos os documentos (tweets)
-def Idf(word, tweets_collection):
-    # (numero total de documentos)/(numero total de documentos que contem o termo)
-    return math.log10(float(len(tweets_collection))/float(NContains(word, t2)))
-
-def TfIdf(word, tweet, tweets_tokenized, tweets_collection): # tf
-    return Tf(word, tweet)*Idf(word, tweets_collection)
-# end
-
-def ClearTweet(arg): # realiza uma limpeza nos tweets, retirando os lixos
+# Clears th tweet, removes 'trash'
+def ClearTweet(arg): 
     arg = re.sub('ç', 'c', arg, flags = re.M)
     arg = re.sub('á|à|â|ã','a', arg, flags = re.M)
     arg = re.sub('é|è|ê','e', arg, flags = re.M)
     arg = re.sub('ì|í|î|ĩ','i', arg, flags = re.M)
     arg = re.sub('ò|ó|ô|õ','o', arg, flags = re.M)
     arg = re.sub('-', '', arg, flags = re.M)
-    arg = re.sub('(<[^>]+>)|(@[\w]+)|(\#+[\w_]+[\w\'_\-]*[\w_]+)|(\d+)|(http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+)|([:=;][oO\-]?[D\)\]\(\]/\\OpP])|(h[t]+)', '', arg, flags = re.M)
-    #('<[^>]+>) remove HTMLs;
-    #('@[\w]+) remove mentions;
-    #('\#+[\w_]+[\w\'_\-]*[\w_]+) remove hashtags;
-    #('\d+','', arg, flags = re.M) remove numeros;
-    #(http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+
-    #...remove URLs;
-    #([:=;][oO\-]?[D\)\]\(\]/\\OpP]) ignora smiles
+    
+    # Removes HTML: ('<[^>]+>)
+    # Removes mentions: ('@[\w]+)
+    # Removes hashtags: ('\#+[\w_]+[\w\'_\-]*[\w_]+)
+    # Removes numbers: ('\d+','', arg, flags = re.M)
+    # Removes URLs: (http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|
+    # [!*\(\),]|(?:%[0-9a-f][0-9a-f]))+
+    # Ignores smiles: ([:=;][oO\-]?[D\)\]\(\]/\\OpP])
+    arg = re.sub('(<[^>]+>)|(@[\w]+)|(\#+[\w_]+[\w\'_\-]*[\w_]+)|(\d+)|(http[s]?
+    ://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+)|([:=;][oO\-]?
+    [D\)\]\(\]/\\OpP])|(h[t]+)', '', arg, flags = re.M)
     return arg
 
-# Tokeniza por palavras
+# Tokenize words
 def tokenize(arg): 
     return tokens_re.findall(arg)
 
-# Stemizacao por palavra (Stemmizar a partir da tokenizacao(?))
+# Stemize words
 def stem(arg): 
     lowercase = arg.lower()
     
@@ -82,149 +70,155 @@ def stem(arg):
     else:
         return stem_pt
     
-
-regex_str = [r'^[<[^>]+>]', # ignora HTML tags
-            r'^[@[^.]+]', # ignora mentions
-            r"^(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # ignora hash-tags
+# Regular expression set to ignores following patterns
+regex_str = [r'^[<[^>]+>]', # ignores HTML tags
+            r'^[@[^.]+]', # ignores mentions
+            r"^(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # ignores hash-tags
             r"""^http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|
-            (?:%[0-9a-f][0-9a-f]))+""", # ignora URLs
-            r'^(?:(?:\d+,?)+(?:\.?\d+)?)', # ignora numbers
-            r"(?:[a-z][a-z'\-_]+[a-z])" ] # agrupa palavras
+            (?:%[0-9a-f][0-9a-f]))+""", # ignores URLs
+            r'^(?:(?:\d+,?)+(?:\.?\d+)?)', # ignores numbers
+            r"(?:[a-z][a-z'\-_]+[a-z])" ] # agrupes palavras
 
+# Compiles regex
 tokens_re = re.compile(r'('+'|'.join(regex_str)+')',
-                       re.VERBOSE | re.IGNORECASE) # compila expressao regular
-# Limpa a lista de stop words
+                       re.VERBOSE | re.IGNORECASE)
+
+# Applies regex to stop-words list
 stop_words = [ClearTweet(str(word.encode(encoding='UTF-8', errors='strict')))
               for word in NL.corpus.stopwords.words('portuguese')]
 
-time_begin = time.asctime(time.localtime(time.time())) # begin
+# Start
+time_begin = time.asctime(time.localtime(time.time()))
 
-#
-
-# Colecao com todas as seguradoras e seus seguidores
-# Eh um dictionary onde as chaves sao as seguradoras 
-# e os valores sao os seguidores dessa seguradora
+# Collection with all insurance-companies and it's followers
+# >U< is for Universe, which is a dict which keys are insurance-companies
+# and the values are the followers
 U = dbm.GetUniverse()
 
-for seguradoraId in U.keys():
-    print "seguradoraId: %s" % dbm.GetAccountLabel(seguradoraId)
+for insurance_id in U.keys():
+    print "insurance_id: %s" % dbm.GetAccountLabel(insurance_id)
     
-    # followersFeed eh um dict onde as chaves sao os seguidores
-    #...e os valores sao os ids dos tweets desses seguidores
-    followersFeed = dbm.GetFollowerTweets(U, seguradoraId)
+    # >followers_feed< is a dict data type where keys are followers
+    # and values are tweet ids from these followers
+    follower_feed = dbm.GetFollowerTweets(U, insurance_id)
     
-    # Remove seguidores nao tem tweets inseridos
-    followersFeed = dict((k, v) for k, v in followersFeed.iteritems() if v)
+    # Removes followers with no tweets inserted
+    follower_feed = dict((k, v) for k, v in follower_feed.iteritems() if v)
     
     errfd = open("ERR.txt", "w")
 
-    preprocessedTweetsBySeg = dict()
-    R = dict() # R : conjunto de raizes e suas derivacoes
+    # Dict used to create the .csv file
+    # Keys are tweet ids and values are the whole 
+    # stopwordless-stemmed-tokenized tweets from every single follower from the
+    # current insurance-company
+    collection_of_tweets = dict()
     
-    for i, follower in enumerate(followersFeed.keys()):
-        
-        #t1 = [] # t1 : tweet tokenizados
+    # >root_derivations< is a dict which keys are root tokens (stem) and
+    # values are derivations from that stem. i.e, {r, [w1, w2, ..., wK]}
+    # Where r ε {stem(w) | w belongs to >basket_of_terms<}
+    # and w ε >possible_roots<. i.e. {'work', ['worked', 'working']}
+    R = dict()
+    
+    for i, follower in enumerate(follower_feed.keys()):
+        # >t1< is a dict where keys are tweet ids and values are 
+        # tokenized tweet contents
         t1 = dict()
-        t2 = dict()
-        t3 = dict() # t3 : tweet preprocessado (sem stopword e tokenizado)
         
-        # 1. tokeniza tweets
-        print "\tSeguidor %s" % dbm.GetAccountLabel(follower)
-        print "\t\tTokenizando..."
+        # >t2< is a dict where keys are tweet ids and values are
+        # tokenized tweet contents without stop-words
+        t2 = dict()
+        
+        # >t3< is a dict where keys are tweet ids and values are
+        # stemmed-tokenized tweet contents
+        t3 = dict()
+        
+        # 1. Tokenize tweets
+        print "\Insurance-company %s" % dbm.GetAccountLabel(follower)
+        print "\t\tTokenizing..."
         try:
-            #  conjunto de tweets tokenizados
-            for tweet in followersFeed[follower].keys():
-                #print tweet, followersFeed[follower][tweet]
+            # Tokenized tweets set
+            for tweet in follower_feed[follower].keys():
                 t1[tweet] = tokenize(ClearTweet(\
-                    str(followersFeed[follower][tweet].encode(\
+                    str(follower_feed[follower][tweet].encode(\
                     encoding='UTF-8', errors='strict'))))
         except(KeyError):
-            errfd.write("[Erro na tokenizacao]\n");
+            ?
             break
         
-        # 2. Retira stopwords
-        # t2 : conjunto de tweets tokenizados sem stopwords
+        # 2. Removes stop-words
         try:
-            print "\t\tRemovendo stopwords ..."
+            print "\t\tRemoving stop-words ..."
             for tweetId, tweet in t1.iteritems():
                 t2[tweetId] = list(set(tweet) - set(stop_words))
-                ##t2[tweetId] = [word for word in set(tweet)-set(stop_words)]
         except(KeyError):
-            errfd.write("[Erro na remocao de stopwords]\n")
+            ?
             break
         
-        # 2.1. Preenche conjunto com os tweets
-        # Rp (Raizes possiveis) : conjunto com todas...
-        #...possiveis palavras que foram stemizadas
-        Rp = [word for k, tweet in t2.iteritems() for word in tweet]
+        # 2.1. Fills set with tweets
+        # >possible_roots< is a possible root list where values are tokens from 
+        # the stop-wordless-tokenized tweets
+        possible_roots = [word for k, tweet in t2.iteritems() for word in tweet]
         
-        # 2.2 Stemiza tweets
+        # 2.2 Stemize tweets
         try:
-            print "\t\tStemizando..."
+            print "\t\tStemizing..."
             for tweetId, tweet in t2.iteritems():
                 t3[tweetId] = [stem(word) for word in tweet]
         except(KeyError):
-            errfd.write("[Erro na stemizacao]\n")
+            ?
             break
         
         
-        # 3. Retira do conjunto de todas palavras possivelmente...
-        #...stemizadas aquelas que nao foram stemizadas
+        # 3. Removes from the possible root list those
+        # tokens which weren't stemmized
         for k, tweet in t3.iteritems():
-            Rp = list(set(Rp) - set(tweet))
-        Rp = list(set(Rp))
+            possible_roots = list(set(possible_roots) - set(tweet))
+        possible_roots = list(set(possible_roots))
         
-        # 3.1 Converte os termos em seus respectivos stems
+        # 3.1 Converts tokens to their stems
         s = stem
-        Rp = map(lambda x: s(x), Rp)
+        possible_roots = map(lambda x: s(x), possible_roots)
         
-        # 3.2 Inicializa o dict colocando os stems nas chaves
-        for _ in Rp:
+        # 3.2 Starts dict inserting stems in keys
+        for _ in possible_roots:
             R.update({_: [],})
-            
-        # 4. Preenche conjunto de pares {r: [w1, w2, ..., wK]} em R,
-        #...onde r ε {stem(w) | w pertence a R} e wK ε Rp
-        basketOfTerms = list(set([_ for tweet in t2.values() for _ in tweet]))
-               
         
+        
+        basket_of_terms = list(set([_ for tweet in t2.values() for _ in tweet]))
+        
+        
+        # 4. Fills pair set {r: [w1, w2, w3, ..., wK]} in R
+        # where r ε {stem(w) | w belongs to R} and wK ε possible_roots
         try:
-            maxiter = len(basketOfTerms)
-            print "\t\tPreenchendo hash de raiz x possiveis palavras..."
-            print '\t\tNumero de iteracoes: %d' % maxiter
-            #s = stem
-            """d = time.clock()
-            for word in Rp:
-                R[s(word)] = map(lambda x: x, filter(lambda x: s(x) == s(word), basketOfTerms))
-            e = time.clock()
-            print 'old approuch took ', e-d"""
+            maxiter = len(basket_of_terms)
+            print '\t\tFilling root x possible derivations dictionary'
+            print '\t\tNumber of iterations: %d' % maxiter
             
-            for word in basketOfTerms:
+            for word in basket_of_terms:
                 wordstemmed = stem(word)
                 if wordstemmed in R.keys():
                     R[stem(word)] += [word,]
                     
-            print "\t%d de %d"\
-                % (i+1, len(followersFeed.keys()))
+            print "\t%d of %d"\
+                % (i+1, len(follower_feed.keys()))
         except(KeyError):
-            errfd.write("[Erro ao preencher hash de raizes]")
+            ?
             break
         
-        #BEGIN - INSERCAO NO BANCO
+        # Starts inserting in database
         dbm.InsertPreprocessedTweets(t3)
-        dbm.InsertRoot(followersFeed[follower], R, t2, seguradoraId)
+        dbm.InsertRoot(follower_feed[follower], R, t2, insurance_id)
         #END
         
         for k, v in t3.iteritems():
             if v:
-                preprocessedTweetsBySeg[str(k)] = v
+                collection_of_tweets[str(k)] = v
                     
-    # 5. Gerar arquivo no formato documentoIdXpalavra,
-    #...onde palavra ε t2 e documentoID ε coleção
-    #print "Preprocessed tweets by seg: ", preprocessedTweetsBySeg
-
+    # 5. Genarates the output file in document_idXtoken format
+    # where document_id ε collection and token ε t2  
     data = dict()
     
-    for tweet in preprocessedTweetsBySeg.values():
+    for tweet in collection_of_tweets.values():
         if tweet:
             for word in R.keys():
                 if word in data.keys():
@@ -237,54 +231,18 @@ for seguradoraId in U.keys():
     
     import os
     
-    label = dbm.GetAccountLabel(seguradoraId)
+    label = dbm.GetAccountLabel(insurance_id)
     
     if not os.path.isdir('../analytics/%s' % label):
         os.mkdir('../analytics/%s' % label)
         
-    label = dbm.GetAccountLabel(seguradoraId)
+    label = dbm.GetAccountLabel(insurance_id)
     data_frame.to_csv('../analytics/%s/%s.csv' % (label, label),\
-        index_label = ['',]+preprocessedTweetsBySeg.keys())
-
-    """
-    try:
-        print "\t\tGerando arquivo de saida..."
-        temp = []
-        scores = ""
-        for term in R.keys():
-            temp.append(term)
-            scores += str(term)+";"
-            #print term
-        scores += "\n"
-        for tweetId in preprocessedTweetsBySeg.keys():
-            #print tweetId, preprocessedTweetsBySeg[tweetId]
-            if len(preprocessedTweetsBySeg[tweetId]) != 0:
-                scores += str(tweetId)+";"
-                for word in temp:
-                    if (word in preprocessedTweetsBySeg[tweetId]):
-                        scores += str(Tf(word, preprocessedTweetsBySeg[tweetId]))+";"
-                    else:
-                        scores += "0;"
-                scores += "\n"
-        #
-        arq = open('../scores/'+str(seguradoraId)+'.txt', 'w')
-        arq.write(scores)
-        arq.close()
-      
-    except(KeyError):
-        errfd.write("[Erro ao gerar arquivo de saida]")
-        break
-    """
+        index_label = ['',] + collection_of_tweets.keys())
     
-    print "\tSeguradora "+str(seguradoraId)+" cadastrada!\n"
-    errfd.close()
+    print '\t%s insurance-company inserted!\n' % dbm.GetAccountLabel(insurance_id)
 
-# insere tweets preprocessados no DB
-#ins ert_preprocessed_tweets(tweets_preprocessed, ids_collection)
-
-print "\nPREPROCESSAMENTO FINALIZADO"
-print "Coleções preprocessadas : "+str(len(U))
-print "Iniciado as: "+time_begin
-print "Finalizado as: "+time.asctime(time.localtime(time.time()))+"\n"
-#Done
-
+print '\nPreprocessing done!'
+print 'Collections inserted: %d' % len(U)
+print 'Started at ', time_begin
+print "Finished at ", time.asctime(time.localtime(time.time()))
