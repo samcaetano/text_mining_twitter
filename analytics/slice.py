5291f14773ff492c1a8a87162cf99b8b2c1b4677
Samuel Caetano <sam.kaetano@gmail.com>
2017-07-14 00:01:56 -0300

    Add files via upload

--- analytics/slice.py
+++ analytics/slice.py
@@ -10,7 +10,7 @@
 # External imports
 import pandas as pd
 import numpy as np
-import sys
+import sys, os
 
 # Internal imports
 sys.path.insert(0, '../lib')
@@ -27,49 +27,49 @@ def PlotDataFromFile(insurance_id):
     df = pd.read_csv('%s/%s.csv' % (insurance_label, insurance_label))
     
     # Drops all NaNs columns
-    df = df.dropna(axis = 'columns',\
-        how = 'all')
+    df = df.dropna(axis = 'columns', how = 'all')
 
     # Calculates the occurence frequencies of the term in the documents
+    M = pd.DataFrame.as_matrix(df)
+    
     terms = []
     terms_freq = []
-    for row in df.index:
+    
+    for row in M:
+        terms.append(row[0])
+        terms_freq.append(sum(list(row[1:])))
         
-        occurences = 0
-        
-        for column in df.columns[1:]:
-            occurences += df[column][row]
-        
-        terms += [df['Unnamed: 0'][row],]
-        terms_freq += [occurences,]
-        
+    del M
+    
     # Pairs term and it's frequencies
     zipped = zip(terms, terms_freq)
     
     # Sorts decrescently terms with it's frequencies
-    zipped_sorted = sorted(zipped, key = lambda t: t[1],\
-        reverse = True)
+    zipped_sorted = sorted(zipped, key = lambda t: t[1],
+                           reverse = True)
     
     # Data array
     data = np.array([_[1] for _ in zipped_sorted])
     data_labels = [_[0] for _ in zipped_sorted]
     
     # Finds the quartiles and median
-    q1, medians, q3 = np.percentile(data, [25, 50, 75])
+    q1, median, q3 = np.percentile(data, [25, 50, 75])
     
-    print q1, medians, q3
+    print '1st Quartile', q1
+    print 'Median ',  median
+    print '3rd Quartile', q3
     
     # Draws the bars chart. Term per occurence
     plt.figure(1)
-    plt.bar(np.arange(len(data_labels)),\
-            data,\
-            align = 'center',\
+    plt.bar(np.arange(len(data_labels)),
+            data,
+            align = 'center',
             alpha = 0.5)
     
     plt.xlabel('Terms')
     plt.ylabel('Occurences')
-    plt.title('Occurrence of terms in documents of %s'\
-        % insurance_label)
+    plt.title('Occurrence of terms in documents of %s'
+              % insurance_label)
     plt.savefig('%s/%s_ocorrenceTerms.png'\
         % (insurance_label, insurance_label))    
     plt.close()
@@ -77,8 +77,8 @@ def PlotDataFromFile(insurance_id):
     # Desenha o boxplot
     # Draws the boxplot
     plt.figure(2)
-    plt.title('Boxplot of %s'\
-        % insurance_label)
+    plt.title('Boxplot of %s'
+              % insurance_label)
     bp = plt.boxplot(data)
     plt.savefig('%s/%s_boxplot.png'\
         % (insurance_label, insurance_label))
@@ -86,58 +86,88 @@ def PlotDataFromFile(insurance_id):
     
     # Draws the violin
     plt.figure(3)
-    plt.title('Density and occurrence of terms in docs from %s'\
-        % insurance_label)
+    plt.title('Density and occurrence of terms in docs from %s'
+              % insurance_label)
     plt.xlabel('Density')
     plt.ylabel('Occurrences')
-    plt.violinplot(data,\
-        showmeans = True,\
-        showmedians = True,\
-            showextrema = True)
-    plt.savefig('%s/%s_violinplot.png'\
-        % (insurance_label, insurance_label))
+    plt.violinplot(data, 
+                   showmeans = False,
+                   showmedians = True)
+    plt.savefig('%s/%s_violinplot.png'
+                % (insurance_label, insurance_label))
     
-    CutFile(q1, q3, zipped, df, insurance_label)
+    CutFile(q3, zipped, df, insurance_label)
     
     plt.close()
+    del df
     
-def CutFile(quartile1, quartile3, data, df, label):
-    
+def CutFile(q3, data, df, label):
     print 'Cutting %s file' % label
     
-    sequence_to_remove = []
+    # Itera entre os valores que sao menores do que o quartil 3
+    sequence_to_remove = [index for index, v in enumerate(data) if v[1] < q3]
     
-    for i, _ in enumerate(data):
-        if _[1] < quartile3:
-            sequence_to_remove += [i,]
-    
+    # Removes terms who has freqs below the quartile3
     df = df.drop(df.index[sequence_to_remove])
     
-    sequence_to_remove = []
-    for c in df.columns:
-        if c != 'Unnamed: 0':
-            acc = 0.
-            for r in df.index:
-                acc += df[c][r]
-            if acc == 0.:
-                print c, ' removed'
-                #sequence_to_remove += [c,]
-                df = df.drop(c, axis = 1)
+    M = pd.DataFrame.as_matrix(df)
     
-    df.to_csv('%s/%s_sliced.csv'\
-        % (label, label))
+    column_to_remove = []
+    for i, column in enumerate(M.T):
+        try:
+            if sum(list(column)) == 0:
+                column_to_remove.append(i)
+        except(TypeError):
+            next
+
+    del M
+    
+    column_names = [df.columns[index] for index in column_to_remove]
+    for i, name in enumerate(column_names):
+        try:
+            df = df.drop(str(name), axis = 1)
+            print name, ' deleted @ ', i
+        except(IndexError):
+            print 'erro em ', name
+    
+    df.to_csv('%s/%s_sliced.csv' % (label, label))  
+    del df
+    
+    # TESTE v
+    df = pd.read_csv('%s/%s_sliced.csv' % (label, label))
+    M = pd.DataFrame.as_matrix(df)
+    print M
+    for i, column in enumerate(M.T):
+        try:
+            if sum(list(column)) == 0:
+                print 'erro'
+        except(TypeError):
+            next
+            
+    del M
+    del df
+    #TESTE ^
+    
     
 general_insurances = dbm.GetAllSeguradoras()
 
 for insurance_id in general_insurances:
+    #insurance_id = 1202130601
     try:
-        PlotDataFromFile(insurance_id)
+        label = dbm.GetAccountLabel(insurance_id)
+        if os.path.isfile('%s/%s_sliced.csv' % (label, label)):
+            print '%s already sliced' % label
+            next
+        else:
+            print 'Sliced %s started' % label
+            PlotDataFromFile(insurance_id)
+            print "Slice done"
     except(IOError):
         next
     except(KeyError), e:
-        with open("ERR.txt", "w") as arq:
+        with open("message.err", "w") as arq:
             arq.write("\n[KeyError slicing the file]\n")
+            arq.close()
         print "\t\tError reducing dimensionality"
         print e
-    print "\t\tSlice done"
-    
+    break
